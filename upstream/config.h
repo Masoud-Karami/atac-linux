657f0bf3c7d8f851c4bf2e0df1830987b99242a3
Masoud-Karami <massoud.karami@gmail.com>
2026-05-05 09:58:33 -0400

    install dev-tools/gcc/Config linux-gnu

---
+++ upstream/config.h
@@ -0,0 +1,35 @@
+/* config.h.  Generated automatically by configure.  */
+/* $Id: config_h.in,v 3.2 1997/12/10 01:51:44 tom Exp $ */
+
+#define HAVE_ATEXIT 1
+#define HAVE_FCNTL_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_ON_EXIT 1
+#define HAVE_RENAME 1
+#define HAVE_STDARG_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRERROR 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TIME_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_WAIT_H 1
+#define HAVE_TIME_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE_VFPRINTF 1
+#define M_TERMINFO 1
+#define OUTC_RETURN 1
+#define RETSIGTYPE int
+#define STDC_HEADERS 1
+#define SYSTEM_NAME "linux-gnu"
+#define TARGET_BELL 007
+#define TARGET_BS 010
+#define TARGET_CR 015
+#define TARGET_FF 014
+#define TARGET_NEWLINE 012
+#define TARGET_TAB 011
+#define TARGET_VT 013
+#define TIME_WITH_SYS_TIME 1
